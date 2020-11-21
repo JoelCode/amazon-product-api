d3a31d5ecdda5786f54aa677e1c0f5d2435ee099
JoelCode <joelturcotte.g@gmail.com>
2020-11-21 14:01:03 +0700

    code cleaning

--- jest.config.js
+++ jest.config.js
@@ -4,6 +4,5 @@ module.exports = {
     '^.+\\.[tj]sx?$': 'ts-jest',
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'html'],
-  coverageDirectory: '../../coverage/libs/data-amazon-product',
   setupFiles: ['./environments/development.js']
 };
--- tests/adapter-amazon-buddy.spec.ts
+++ tests/adapter-amazon-buddy.spec.ts
@@ -2,6 +2,7 @@ import {asin} from '../lib';
 import {inspect} from 'util';
 
 describe('adapterAmazonBuddy', () => {
+    // @ts-ignore
     it('asin B07MGMGH9D', async function () {
         const res = await asin({asin: 'B07MGMGH9D'});
         console.log(inspect(res, false, null));
